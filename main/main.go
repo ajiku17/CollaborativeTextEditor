97a11d4a959c2e5def188f161dae68356789fd85
Aleksandre Jikuridze <ajiku17@freeuni.edu.ge>
2021-06-13 04:11:01 +0400

    document and position_manager interfaces; major refactoring #8 #9

--- main/main.go
+++ main/main.go
@@ -2,39 +2,31 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"testing"
+	"log"
 
 	"github.com/crdt"
 )
 
 func main() {
-	doc := crdt.NewDocument()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	doc.InsertAt("H", 0, 1)
-	doc.InsertAt("e", 1, 1)
-	doc.InsertAt("l", 2, 1)
-	doc.InsertAt("l", 3, 1)
-	doc.InsertAt("o", 4, 1)
-	doc.InsertAt(" ", 5, 1)
-	doc.InsertAt("W", 6, 1)
-	doc.InsertAt("o", 7, 1)
-	doc.InsertAt("r", 8, 1)
-	doc.InsertAt("l", 9, 1)
-	doc.InsertAt("d", 10, 1)
+	doc := new(crdt.BasicDocument)
+	positionManager := new(crdt.BasicPositionManager)
+
+	positionManager.PositionManagerInit()
+	doc.DocumentInit(positionManager)
+
+	doc.InsertAtIndex("H", 0, 1)
+	doc.InsertAtIndex("e", 1, 1)
+	doc.InsertAtIndex("l", 2, 1)
+	doc.InsertAtIndex("l", 3, 1)
+	doc.InsertAtIndex("o", 4, 1)
+	doc.InsertAtIndex(" ", 5, 1)
+	doc.InsertAtIndex("W", 6, 1)
+	doc.InsertAtIndex("o", 7, 1)
+	doc.InsertAtIndex("r", 8, 1)
+	doc.InsertAtIndex("l", 9, 1)
+	doc.InsertAtIndex("d", 10, 1)
 
 	fmt.Println(doc.ToString())
-
-	result := testing.Benchmark(func(parentB *testing.B) {
-		parentB.Run("example", func(b *testing.B) {
-			fmt.Printf("ok")
-		})
-	})
-
-	fmt.Printf("%v", result)
-
-	empty_benchmark := testing.BenchmarkResult{}
-	if reflect.DeepEqual(result, empty_benchmark) {
-		fmt.Printf("aaa")
-	}
 }
